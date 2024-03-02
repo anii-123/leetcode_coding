0619801284b3e0570d227515ab5dd9b888946af6
anii-123 <142479716+anii-123@users.noreply.github.com>
2024-03-02 22:35:30 +0530

    Time: 3 ms (99.27%) | Memory: 42.9 MB (15.92%) - LeetSync

---
+++ 2277-count-equal-and-divisible-pairs-in-an-array/count-equal-and-divisible-pairs-in-an-array.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int countPairs(int[] nums, int k) {
+        int count=0;
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]==nums[j]){
+                  int l=i*j;
+                
+                 if(l%k==0){
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+}
