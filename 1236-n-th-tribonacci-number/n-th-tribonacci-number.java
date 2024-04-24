78e252bd0cec27e2756e117487d2345d2d6106c3
anii-123 <142479716+anii-123@users.noreply.github.com>
2024-04-24 20:29:33 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (44.93%) - LeetSync

---
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int tribonacci(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1 || n == 2) {
+            return 1;
+        } 
+        
+        int[] h = new int[n + 1];
+        h[1] = 1;
+        h[2] = 1;
+        
+        for (int i = 3; i <= n; i++) {
+            h[i] = h[i - 1] + h[i - 2] + h[i - 3];
+        }
+        
+        return h[n];
+    }
+}
