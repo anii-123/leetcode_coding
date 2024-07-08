a305ee23c01d356c6f14c89a3a8e2a03299cf0bc
anii-123 <142479716+anii-123@users.noreply.github.com>
2024-07-08 22:32:26 +0530

    Time: 0 ms (100.00%) | Memory: 39.8 MB (97.40%) - LeetSync

---
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int findTheWinner(int n, int k) {
+        int count=0;
+        for(int i=1;i<=n;i++){
+            count=(count + k)% i;
+        }
+        return count+1;
+
+
+        // int t=n-k;
+        // return t;
+
+    }
+}
