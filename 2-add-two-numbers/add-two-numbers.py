49b71dd5787cb057bd5b0516f25ebddcec348265
ANISHA PRAKASH GURUMUKHI <142479716+anii-123@users.noreply.github.com>
2026-07-27 19:59:26 +0530

    Time: 10 ms (36.32%) | Memory: 12.5 MB (57.45%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -10,34 +10,71 @@ class Solution(object):
         :type l2: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
-        temp = ListNode(0)
-        current = temp
-        c = 0
+        # res = ListNode(0)
+        # current = res
+        # total=0
+        # c = 0
+        # while l1 or l2 or c:
+        #     #x = l1.val if l1 else 0
+        #     #y = l2.val if l2 else 0
+        #     if l1:
+        #         x=l1.val
+    
+        #     else:
+        #         x=0
+                
 
-        while l1 or l2 or c:
-            #x = l1.val if l1 else 0
-            #y = l2.val if l2 else 0
+        #     if l2:
+        #         y=l2.val
+                
+        #     else:
+        #         y=0
+               
+
+        #     total = x + y + c
+        #     c = total // 10
+        #     current.next = ListNode(total % 10)
+        #     current = current.next
+        #     if l1:
+        #         l1 = l1.next
+        #     if l2:
+        #         l2 = l2.next
+        # return res.next
+
+
+        res=ListNode()
+        temp=res
+        total=0
+        carry=0
+        while l1 or l2 or carry:
             if l1:
-                x=l1.val
+                num1=l1.val
             else:
-                x=0
-
+                num1=0
+            
             if l2:
-                y=l2.val
+                num2=l2.val
             else:
-                y=0
-        
+                num2=0
+            total= num1+num2+carry
+            carry=total//10
+            total= total%10
 
-            total = x + y + c
-            c = total // 10
-
-            current.next = ListNode(total % 10)
-            current = current.next
+            temp.next=ListNode()
+            temp=temp.next
+            temp.val=total
 
             if l1:
-                l1 = l1.next
+                l1=l1.next
             if l2:
-                l2 = l2.next
+                l2=l2.next
+        return res.next
+           
 
-        return temp.next
+
+
+
+
+
+    
         
